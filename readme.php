e6d8cf1321b21a70c0cdeac8f0b3b10457933ff5
JP69 <jp.div69@gmail.com>
2014-11-07 12:58:50 +0100

    Ajout fichier readme.php

---
+++ readme.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <META http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <title>Partager un repository sur GitHub</title>
+    </head>
+    <body> 
+        <?php
+            echo "Partage_GitHub<br>";
+            echo "<br>";
+            echo "Ce Repository a &eacutet&eacute cr&eacute&eacute dans le cadre de l'activit&eacute 'Partagez un Repository sur GitHub'<br>";
+            echo "de la formation Openclassrooms 'G&eacuterer son code avec Git et GitHub'<br>";
+            echo "(http://openclassrooms.com/courses/gerer-son-code-avec-git-et-github).<br>";
+            echo "<br>"
+            echo "Il ne contient aucun code ni aucune informaton utile.";
+        ?>
+    </body>
+</html>
